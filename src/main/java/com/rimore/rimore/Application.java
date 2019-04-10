9fed283a06da57e0b1f5088bb8ad005845f4f397
Erwin Douna <erwin.douna@outlook.com>
2019-04-10 15:36:34 +0200

    Changes

---
+++ src/main/java/com/rimore/rimore/Application.java
@@ -0,0 +1,17 @@
+package com.rimore.rimore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    /**
+     * Main start function
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
