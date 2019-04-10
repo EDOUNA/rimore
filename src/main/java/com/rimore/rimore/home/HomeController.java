d597b8516042d269e9d63fb7b8605eab35baba18
Erwin Douna <erwin.douna@outlook.com>
2019-04-10 15:46:42 +0200

    Fixing first build

--- src/main/java/com/rimore/rimore/home/HomeController.java
+++ src/main/java/com/rimore/rimore/home/HomeController.java
@@ -1,5 +1,6 @@
 package com.rimore.rimore.home;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
