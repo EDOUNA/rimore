1fc8b2b84f151b5f97622a9fe9ae4509df776d41
Erwin Douna <e.douna@gmail.com>
2019-04-10 22:00:45 +0200

    FInalizing data model for today

--- src/main/java/com/rimore/persistence/models/AgendaRequestsModel.java
+++ src/main/java/com/rimore/persistence/models/AgendaRequestsModel.java
@@ -3,6 +3,7 @@ package com.rimore.persistence.models;
 import java.sql.Timestamp;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "agenda_requests")
@@ -29,15 +30,15 @@ public class AgendaRequestsModel {
     }
 
     @Column(nullable = false, name = "start_date")
-    @NotBlank(message = "Start date is required")
+    @NotNull(message = "Start date is required")
     public Timestamp startDate;
 
     @Column(name = "end_date")
-    @NotBlank(message = "End date is required")
+    @NotNull(message = "End date is required")
     public Timestamp endDate;
 
     @Column(nullable = false, name = "user_id")
-    @NotBlank(message = "UserId is required")
+    @NotNull(message = "UserId is required")
     public Integer userId;
 
     @Column
